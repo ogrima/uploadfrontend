a89e59b17324f3a99b0a0b61a45c56801a3540ba
Leandro Grimaldi <lbgrimaldi@gmail.com>
2019-09-01 22:11:53 -0300

    Commit Inicrial.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Main from './components/Main';
+
+const App = () => (
+  <div>
+    <h1>Upload do Plano de Teste.</h1>
+    <Main />
+  </div>
+);
+
+export default App;
