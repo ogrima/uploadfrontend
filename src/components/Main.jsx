f23b5f721d12fbdf7851a554dd406a4e7607b561
Leandro Grimaldi <leandro.grimaldi@sulamerica.com.br>
2019-09-03 16:10:25 -0300

    url com hhtps

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -20,12 +20,12 @@ class Main extends React.Component {
     data.append('file', this.uploadInput.files[0]);
     data.append('filename', this.fileName.value);
     //data.append('filename', this.refs.fileName.input.value);
-    fetch('http://uploadbackend:8000/upload', {
+    fetch('https://uploadbackend:8000/upload', {
       method: 'POST',
       body: data,
     }).then((response) => {
       response.json().then((body) => {
-        this.setState({ imageURL: `http://uploadbackend:8000/${body.file}` });
+        this.setState({ imageURL: `https://uploadbackend:8000/${body.file}` });
       });
     });
   }
